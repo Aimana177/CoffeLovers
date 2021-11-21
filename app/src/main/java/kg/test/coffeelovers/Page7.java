5f6d31b170ca8311b624ba8cfba1d7531ca23d71
aimanabolotbekova <bolotbekova_a@auca.kg>
2021-11-21 21:21:45 +0600

    1.5

---
+++ app/src/main/java/kg/test/coffeelovers/Page7.java
@@ -0,0 +1,6 @@
+package kg.test.coffeelovers;
+
+import android.app.Activity;
+
+public class Page7 extends Activity {
+}
---
+++ app/src/main/java/kg/test/coffeelovers/page6.java
@@ -0,0 +1,6 @@
+package kg.test.coffeelovers;
+
+import android.app.Activity;
+
+public class page6 extends Activity {
+}
